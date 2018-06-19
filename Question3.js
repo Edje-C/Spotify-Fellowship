7e8369697942e9f61759d9d3ef3a977f86c11867
Elon Jefferson <elekson9@yahoo.com>
2018-06-18 21:12:17 -0400

    Question 3 complete

--- Question3.js
+++ Question3.js
@@ -1,9 +1,56 @@
 const changePossibilities = (amount, denominations) => {
+	let arr = []
 
+	if(denominations.length === 0) {
+		return arr
+	}
+
+	if(denominations.length === 1) {
+		let value = denominations[0]
+		if(!(amount%value)) {
+			let straight = []
+			let sum = 0
+			while(amount > sum) {
+				straight.push(value)
+				sum += value
+			}
+			arr.push(straight)
+		}
+		return arr
+	}
+
+	let max = denominations[denominations.length-1]
+	let diff = amount-max
+
+	if(max === amount) {
+
+		arr.push([max])
+
+	} else if(diff <= max && diff >= denominations[0]) {
+
+		let memo = changePossibilities(diff, denominations.slice(0,-1))
+
+		if(diff === max) {
+			arr.push([max, max])
+		}
+
+		if(memo[0]) {
+			arr.push(...memo.map(v => {
+				v.unshift(max)
+				return v
+			}))
+		}
+	} else if(diff > max) {
+		let memo = changePossibilities(diff, denominations)
+		if(memo[0]) {
+			arr.push(...memo.map(v => {
+				v.unshift(max)
+				return v
+			}))
+		}
+	}
+
+	arr.push(...changePossibilities(amount, denominations.slice(0,-1)))
+
+	return arr
 }
-
-
-result = changePossibilities(4, [1,2,3])
-
-result
-//[[1¢, 1¢, 1¢, 1¢], [1¢, 1¢, 2¢], [1¢, 3¢], [2¢, 2¢]]
